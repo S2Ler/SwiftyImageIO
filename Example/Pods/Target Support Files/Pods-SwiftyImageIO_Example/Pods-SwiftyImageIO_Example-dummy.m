a006a7eeefa66fd8e11d9bc81ecbfdafbb7cfdfc
Alexander Belyavskiy <diejmon@gmail.com>
2016-09-08 23:31:18 +0300

    Remove Pods directory

--- Example/Pods/Target Support Files/Pods-SwiftyImageIO_Example/Pods-SwiftyImageIO_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftyImageIO_Example : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftyImageIO_Example
-@end
--- Example/Pods/Target Support Files/SwiftyImageIO/SwiftyImageIO-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftyImageIO : NSObject
-@end
-@implementation PodsDummy_SwiftyImageIO
-@end
